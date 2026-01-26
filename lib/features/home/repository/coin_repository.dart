cd2e6c07869a5e9a4e61a6768737b348db82461d
walison silva <walisondsi@gmail.com>
2026-01-26 02:12:01 -0300

    refactor(clean): removing unnecessary class

--- lib/features/home/repository/coin_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_crypto_wallet/core/model/coin_model.dart';
-import 'package:flutter_crypto_wallet/core/utils/result.dart';
-
-abstract interface class CoinRepository {
-  Future<Result<List<CoinModel>, Exception>> searchCoins(String query);
-}
--- lib/features/home/service/coin_service.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_crypto_wallet/core/model/coin_model.dart';
-
-abstract interface class CoinService {
-  Future<List<CoinModel>> searchCoins(String query);
-}
